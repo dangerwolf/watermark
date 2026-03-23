1b465ec8a7eae3668ef4a35401e32e3d265d4d0d
WOLF <dangerwolf@users.noreply.github.com>
2026-03-23 16:47:48 +0800

    Update IndexPage.js

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -4,11 +4,6 @@ import Block from "dxc-flex";
 import Main from "./Main/Main";
 import Donation from "./Donation/Donation";
 
-qqShare({
-  title: "身份证盗用所造成的损失，你想象不到！",
-  desc: "这是一款最安全，最快速的纯前端图片加水印，拒绝上传保证个人信息安全。",
-  imgUrl: "https://oss.tuobacco.com/wop2/2018-06-01/5b10dde28a12d.jpg"
-});
 export default class IndexPage extends React.Component {
   render() {
     return (
