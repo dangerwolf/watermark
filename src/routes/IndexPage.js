7b694f5bd7bf88efcbc707cd2f9acc3865c52af6
WOLF <dangerwolf@users.noreply.github.com>
2026-03-23 16:43:07 +0800

    Update IndexPage.js

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -3,7 +3,7 @@ import GitHup from "./GitHup";
 import Block from "dxc-flex";
 import Main from "./Main/Main";
 import Donation from "./Donation/Donation";
-import qqShare from "qq-share";
+
 qqShare({
   title: "身份证盗用所造成的损失，你想象不到！",
   desc: "这是一款最安全，最快速的纯前端图片加水印，拒绝上传保证个人信息安全。",
@@ -29,14 +29,6 @@ export default class IndexPage extends React.Component {
           在各种证件上添加“仅用于办理XXXX，他用无效。”，防止证件被他人盗用！
         </Block>
         <Block horizontal="center">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: "#1b9af7" }}
-            href="https://www.weixin765.com/doc/reeodiqf.html"
-          >
-            新闻：身份证复印件被盗用所造成的损失，你想象不到！
-          </a>
         </Block>
         <Main />
         <Donation />
